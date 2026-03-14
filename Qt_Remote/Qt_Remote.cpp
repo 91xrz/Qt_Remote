ffc3704d12e8d669355362abb546186b53ad4934
Xiong ruizhe <113618883+91xrz@users.noreply.github.com>
2026-03-14 11:15:32 +0800

    实现Qt远程控制主界面布局与基础交互

--- Qt_Remote/Qt_Remote.cpp
+++ Qt_Remote/Qt_Remote.cpp
@@ -1,28 +1,72 @@
 #include "Qt_Remote.h"
 #include "ClientSession.h"
+
 Qt_Remote::Qt_Remote(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
 
-    // 1. 创建逻辑对象
     m_logic = new DeviceServer(this);
 
-    // 2. 连接信号：逻辑层发生什么事，界面层负责显示
-    connect(m_logic, &DeviceServer::logMessage, this, [=](QString msg) {
-        ui.statusBar->showMessage(msg); // 更新状态栏
-        });
+    connect(m_logic, &DeviceServer::logMessage, this, [=](const QString& msg) {
+        ui.statusBar->showMessage(msg);
+        ui.plainTextConnectionLogs->appendPlainText(msg);
+    });
 
-    connect(m_logic, &DeviceServer::clientConnected, this, [=](QString ip, int port) {
-        QMessageBox::information(this, "提示", "发现新连接: " + ip);
-        });
+    connect(m_logic, &DeviceServer::clientConnected, this, [=](const QString& ip, int port) {
+        ui.labelStatusConnectionValue->setText("已连接");
+        ui.labelPeerOnlineValue->setText("在线");
+        ui.plainTextConnectionLogs->appendPlainText(QString("发现新连接: %1:%2").arg(ip).arg(port));
+    });
 
-    // 3. 界面操作逻辑：点击按钮启动服务
-    connect(ui.pushButton, &QPushButton::clicked, this, [=]() {
-        m_logic->startListen(8888);
-        });
+    connect(ui.btnConnect, &QPushButton::clicked, this, [=]() {
+        const int port = ui.lineEditPort->text().toInt();
+        m_logic->startListen(port > 0 ? port : 8888);
+    });
+
+    connect(ui.btnDisconnect, &QPushButton::clicked, this, [=]() {
+        m_logic->stopListen();
+        ui.labelStatusConnectionValue->setText("未连接");
+        ui.labelPeerOnlineValue->setText("离线");
+    });
+
+    connect(ui.btnReconnect, &QPushButton::clicked, this, [=]() {
+        m_logic->stopListen();
+        const int port = ui.lineEditPort->text().toInt();
+        m_logic->startListen(port > 0 ? port : 8888);
+        ui.plainTextConnectionLogs->appendPlainText("执行重连操作");
+    });
+
+    connect(ui.btnStartControl, &QPushButton::clicked, this, [=]() {
+        ui.plainTextPacketLogs->appendPlainText("开始远程控制");
+    });
+
+    connect(ui.btnStopControl, &QPushButton::clicked, this, [=]() {
+        ui.plainTextPacketLogs->appendPlainText("停止远程控制");
+    });
+
+    connect(ui.btnLockMachine, &QPushButton::clicked, this, [=]() {
+        ui.plainTextPacketLogs->appendPlainText("发送控制命令: 锁机");
+    });
+
+    connect(ui.btnUnlockMachine, &QPushButton::clicked, this, [=]() {
+        ui.plainTextPacketLogs->appendPlainText("发送控制命令: 解锁");
+    });
+
+    connect(ui.btnShutdownMachine, &QPushButton::clicked, this, [=]() {
+        ui.plainTextPacketLogs->appendPlainText("发送控制命令: 关机");
+    });
+
+    connect(ui.btnClearLogs, &QPushButton::clicked, this, [=]() {
+        ui.plainTextConnectionLogs->clear();
+        ui.plainTextPacketLogs->clear();
+        ui.plainTextErrorLogs->clear();
+    });
+
+    connect(ui.btnExportLogs, &QPushButton::clicked, this, [=]() {
+        ui.plainTextConnectionLogs->appendPlainText("导出日志: 功能预留");
+    });
 }
 
 Qt_Remote::~Qt_Remote()
 {}
-
